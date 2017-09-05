dc95c1eaaccd73d43f0d66b51b21044d28371e26
Alexandre Escudero <aescudero@sqli.com>
2017-09-05 22:36:54 +0200

    Practice 08 - Solution

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/buffer';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/scan';
 
 {
-    console.log('RxJS - Practice 07');
+    console.log('RxJS - Practice 08');
 
-    const button: Element = document.querySelector('.operators button.commit');
-    const observable: Observable<string> = Observable.fromEvent(button, 'click')
+    const commitButton: Element = document.querySelector('.operators button.commit');
+    const pushButton: Element = document.querySelector('.operators button.push');
+
+    const commitAction$: Observable<string> = Observable.fromEvent(commitButton, 'click')
         .mapTo(1)
         .scan((sum: number, value: number) => sum + value, 0)
-        .map((sum: number) => `Event ${sum}`);
+        .map((sum: number) => `Commit ${sum}`)
+        .do((commit: string) => console.log(commit));
+    const pushAction$: Observable<any> = Observable.fromEvent(pushButton, 'click')
+        .do(() => console.log('Push'));
 
-    observable.subscribe((value: string) => console.log(value));
+    commitAction$
+        .buffer(pushAction$)
+        .subscribe((commits: string[]) => console.log(commits));
 }
 
 
