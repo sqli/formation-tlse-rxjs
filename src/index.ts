1622b08f12e305be2b05a9b6e7d4c39932edaf73
Alexandre Escudero <aescudero@sqli.com>
2017-09-05 22:23:58 +0200

    Practice 03 - Solution

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,32 @@
 import {Observable} from 'rxjs/Observable';
 import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
 
 {
-    console.log('RxJS - Practice 02');
+    console.log('RxJS - Practice 03');
 
-    function generateObserver(max: number) {
-        let index = 0;
-        return {
-            next() {
-                if (index >= max) {
-                    return {
-                        done: true,
-                        value: undefined
-                    };
-                }
-                index++;
-                const nextIndex = index;
-                return {
-                    done: false,
-                    value: {
-                        next: (value: number) => console.log(`Observable ${nextIndex} value: ${value}`),
-                        error: error => console.error(`Observable ${nextIndex} error: ${error}`),
-                        complete: () => console.log(`Observable ${nextIndex} complete`)
-                    }
-                };
-            }
-        };
-    }
-
-    const observerGenerator = generateObserver(100);
+    const getTimestamp = () => Math.floor(new Date().getTime() / 1000);
 
     const observable1: Observable<number> =
-        Observable.create(function subscribe(observer: Observer<number>) {
-            try {
-                observer.next(1);
-                observer.next(2);
-                observer.next(3);
-                observer.complete();
-            } catch (error) {
-                observer.error(error);
-            }
-        });
-    observable1.subscribe(observerGenerator.next().value);
-
+        Observable.create(
+            function subscribe(observer: Observer<number>) {
+                const intervalId = setInterval(
+                    () => observer.next(getTimestamp()), 1000);
+                return () => clearInterval(intervalId);
+            });
     const observable2: Observable<number> =
-        Observable.create(function subscribe(observer: Observer<number>) {
-            try {
-                observer.next(1);
-                observer.next(2);
-                observer.next(3);
-                observer.complete();
-                observer.next(4);
-            } catch (error) {
-                observer.error(error);
-            }
-        });
-    observable2.subscribe(observerGenerator.next().value);
+        Observable.create(
+            function subscribe(observer: Observer<number>) {
+                const intervalId = setInterval(
+                    () => observer.next(getTimestamp()), 1000);
+                return () => clearInterval(intervalId);
+            });
 
-    const observable3: Observable<number> =
-        Observable.create(function subscribe(observer: Observer<number>) {
-            try {
-                observer.next(1);
-                observer.next(2);
-                observer.next(3);
-                observer.complete();
-                observer.error('custom error');
-                observer.next(4);
-            } catch (error) {
-                observer.error(error);
-            }
-        });
-    observable3.subscribe(observerGenerator.next().value);
+    const subscription1: Subscription = observable1
+        .subscribe((value: number) => console.log(`Observer 1: ${value}`));
+    const subscription2: Subscription = observable2
+        .subscribe((value: number) => console.log(`Observer 2: ${value}`));
 
-    const observable4: Observable<number> =
-        Observable.create(function subscribe(observer: Observer<number>) {
-            try {
-                observer.next(1);
-                observer.next(2);
-                observer.next(3);
-                observer.error('custom error');
-                observer.next(4);
-            } catch (error) {
-                observer.error(error);
-            }
-        });
-    observable4.subscribe(observerGenerator.next().value);
-
-    const observable5: Observable<number> =
-        Observable.create(function subscribe(observer: Observer<number>) {
-            try {
-                observer.next(1);
-                observer.next(2);
-                throw new Error('error thrown');
-            } catch (error) {
-                observer.error(error);
-            }
-        });
-    observable5.subscribe(observerGenerator.next().value);
+    subscription1.add(subscription2);
+    setTimeout(() => subscription1.unsubscribe(), 10000);
 }
-
