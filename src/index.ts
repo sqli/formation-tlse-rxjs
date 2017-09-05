e1f93a4a68400aaa9aca88dda3d347f2c2117481
Alexandre Escudero <aescudero@sqli.com>
2017-09-05 22:25:28 +0200

    Practice 04 - Solution

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,10 @@
 import {Observable} from 'rxjs/Observable';
-import {Observer} from 'rxjs/Observer';
-import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/observable/of';
 
 {
-    console.log('RxJS - Practice 03');
+    console.log('RxJS - Practice 04');
 
-    const getTimestamp = () => Math.floor(new Date().getTime() / 1000);
+    const observable: Observable<number> = Observable.of(1, 2, 3, 4);
 
-    const observable1: Observable<number> =
-        Observable.create(
-            function subscribe(observer: Observer<number>) {
-                const intervalId = setInterval(
-                    () => observer.next(getTimestamp()), 1000);
-                return () => clearInterval(intervalId);
-            });
-    const observable2: Observable<number> =
-        Observable.create(
-            function subscribe(observer: Observer<number>) {
-                const intervalId = setInterval(
-                    () => observer.next(getTimestamp()), 1000);
-                return () => clearInterval(intervalId);
-            });
-
-    const subscription1: Subscription = observable1
-        .subscribe((value: number) => console.log(`Observer 1: ${value}`));
-    const subscription2: Subscription = observable2
-        .subscribe((value: number) => console.log(`Observer 2: ${value}`));
-
-    subscription1.add(subscription2);
-    setTimeout(() => subscription1.unsubscribe(), 10000);
+    observable.subscribe((value: number) => console.log(value));
 }
