8b47905932f7f90883930f20095ad97cb1b07bf6
Alexandre Escudero <aescudero@sqli.com>
2017-09-05 22:26:56 +0200

    Practice 05 - Solution

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 
 {
-    console.log('RxJS - Practice 04');
+    console.log('RxJS - Practice 05');
 
-    const observable: Observable<number> = Observable.of(1, 2, 3, 4);
+    const observable: Observable<any> = Observable.of(1, 2, 3, 4)
+        .map((value: number) => {
+            return {
+                value: value,
+                even: value % 2 === 0
+            };
+        });
 
-    observable.subscribe((value: number) => console.log(value));
+    observable.subscribe((value: any) => console.log(value));
 }
+
